27ad3faa258b6596bdb8f7c247a4d9a73c187f3b
Joseph Wright <joseph.wright@morningstar2.co.uk>
2015-08-04 21:02:15 +0100

    Split out catcode table utilities
    As discussed, this will make life easier in terms of having
    multiple interfaces.

---
+++ ctablestack/build.lua
@@ -0,0 +1,23 @@
+#!/usr/bin/env texlua
+
+-- Build script for "ctablestack" files
+
+-- Identify the bundle and module
+bundle = ""
+module = "ctablestack"
+
+checkengines   = {"luatex"} -- no l3build tests yet..
+checkformat    = "lualatex"
+stdengine      = "luatex"
+typesetexe     = "lualatex"
+
+-- Release a TDS-style zip
+packtdszip  = true
+
+-- tests for this bundle
+-- testfiledir = "testfiles"
+
+-- Find and run the build system
+kpse.set_program_name ("kpsewhich")
+dofile (kpse.lookup ("l3build.lua"))
+
